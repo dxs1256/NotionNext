a542b386055ed3da9117e7a52aa0a11635c204e7
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2024-12-10 00:01:51 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -5,6 +5,10 @@ import { siteConfig } from '@/lib/config'
 import SocialButton from './SocialButton'
 import { useState, useEffect } from 'react'
 
+/**
+ * 页脚
+ * @returns
+ */
 const Footer = () => {
   const [ipInfo, setIpInfo] = useState({
     server: { country: '', city: '', ip: '', flag: '' },
@@ -14,7 +18,7 @@ const Footer = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://orrz.alwaysdata.net/ip.php')
+        const response = await fetch('http://example.com/path/to/your/backend.php')  // 替换为实际的后端 PHP 地址
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`)
         }
@@ -41,37 +45,52 @@ const Footer = () => {
   }, [])
 
   return (
-    <footer className="relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm">
+    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm'>
       {/* 颜色过度区 */}
-      <div className="h-32 bg-gradient-to-b from-[#f7f9fe] to-white dark:bg-[#1a191d] dark:from-inherit dark:to-inherit" />
+      <div
+        id='color-transition'
+        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
+      />
 
       {/* 社交按钮 */}
-      <div className="w-full h-24">
+      <div className='w-full h-24'>
         <SocialButton />
       </div>
 
       <br />
 
       {/* 底部页面信息 */}
-      <div className="w-full h-24 flex flex-col lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]">
-        <div id="footer-bottom-left" className="flex flex-col items-center lg:items-start">
+      <div
+        id='footer-bottom'
+        className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
+        <div id='footer-bottom-left'>
           <PoweredBy />
           <CopyRightDate />
         </div>
 
-        <div id="footer-bottom-right" className="flex flex-col md:flex-row justify-center items-center space-y-2 md:space-y-0 md:space-x-4">
+        <div id='footer-bottom-right'>
           {siteConfig('BEI_AN') && (
             <>
-              <i className="fas fa-shield-alt" />
-              <a href="https://beian.miit.gov.cn/" className="mr-2">
+              <i className='fas fa-shield-alt' />{' '}
+              <a href='https://beian.miit.gov.cn/' className='mr-2'>
                 {siteConfig('BEI_AN')}
               </a>
             </>
           )}
           <BeiAnGongAn />
 
-          <div className="flex items-center text-xs md:text-sm mt-2 md:mt-0 space-x-4">
-            <div id="server-info" className="flex items-center space-x-2">
+          <span className='hidden busuanzi_container_site_pv'>
+            <i className='fas fa-eye' />
+            <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
+          </span>
+          <span className='pl-2 hidden busuanzi_container_site_uv'>
+            <i className='fas fa-users' />{' '}
+            <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
+          </span>
+
+          {/* 显示服务器和用户信息 */}
+          <div className='mt-4'>
+            <div id='server-info' className='text-xs md:text-sm'>
               <span>服务器所在地：</span>
               <span>{ipInfo.server.country}</span>
               <img src={ipInfo.server.flag} alt="国旗" className="w-4 h-4" />
@@ -79,7 +98,7 @@ const Footer = () => {
               <span>IP：{ipInfo.server.ip}</span>
             </div>
 
-            <div id="user-info" className="flex items-center space-x-2">
+            <div id='user-info' className='mt-2 text-xs md:text-sm'>
               <span>您的所在地：</span>
               <span>{ipInfo.user.country}</span>
               <img src={ipInfo.user.flag} alt="国旗" className="w-4 h-4" />
